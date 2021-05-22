d2bc9789ffe76c7da93b90fb12311751277aadf5
YiroYi <yiroyi@live.com>
2021-05-22 01:49:11 -0500

    Setting getStaticProps complete

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,17 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: {
-    products: data.products
-  } };
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10
+  };
 }
 
 export default HomePage;
